375fd5878a7de6e6a90a4d128ba6a53b1992df07
silverprojectA <anna.silver22@imperial.ac.uk>
2024-06-13 17:15:55 +0100

    added level meter to sketch.js and implemented in render

--- render.cpp
+++ render.cpp
@@ -1,5 +1,5 @@
 /*
- ____  _____ _        _
+____  _____ _        _
 | __ )| ____| |      / \
 |  _ \|  _| | |     / _ \
 | |_) | |___| |___ / ___ \
@@ -7,77 +7,118 @@
 http://bela.io
 */
 /**
-
+ 
 */
-
+ 
 #include <Bela.h>
 #include <stdexcept>
 #include <libraries/Scope/Scope.h>
 #include <cstdlib>
 #include "TransducerFeedbackCancellation.h"
 #include "audio-utils/au_Biquad.h"
-
+#include <libraries/Gui/Gui.h>
+#include "audio-utils/au_config.h"
+#include <cmath>
+ 
+ 
 Scope scope;
-
+Gui gui;
+ 
+#define INPUT_LOOPBACK_PIN 6
+#define INPUT_VOLTAGE_PIN 2
+#define INPUT_ACTUATION_SIGNAL_PIN 0
+#define OUTPUT_AMP_PIN 2
+#define OUTPUT_LOOPBACK_PIN 6
+#define OUTPUT_PICKUP_SIGNAL_PIN 4
+ 
+#define RESONANT_FREQ_HZ 380.0
+ 
+ 
 TransducerFeedbackCancellation transducer_processing;
-
-
-Biquad smoothing;
-
-
+ 
+ 
+Biquad meter_filter;
+ 
+ 
 bool setup(BelaContext *context, void *userData)
 {
-
+ 
 	TransducerFeedbackCancellation::Setup processing_setup;
-	processing_setup.resonant_frequency_hz = 380.0;
+	processing_setup.resonant_frequency_hz = RESONANT_FREQ_HZ;
 	processing_setup.resonance_peak_gain_db = 23.5;
 	processing_setup.resonance_q = 16.0;
 	processing_setup.resonance_tone_level_db = -10.0;
 	processing_setup.inductance_filter_coefficient = 0.5;
 	processing_setup.transducer_input_wideband_gain_db = 0.0;
 	processing_setup.sample_rate_hz = context->audioSampleRate;
-	
 	transducer_processing.setup(processing_setup);
-	
+	Biquad::FilterSetup metering_lowpass_setup;
+    metering_lowpass_setup.cutoff_freq_hz = 25;
+    metering_lowpass_setup.filter_gain_db = 0;
+    metering_lowpass_setup.quality_factor = 0.5;
+    metering_lowpass_setup.sample_rate_hz = context->audioSampleRate;
+    metering_lowpass_setup.filter_type = Biquad::FilterType::LOWPASS;
+	meter_filter.setup(metering_lowpass_setup);
+
+
 	scope.setup(3, context->audioSampleRate);
-	
+ 
+	// Set up the GUI
+	gui.setup(context->projectName);
+	//Set the buffer to receive from the GUI
+	gui.setBuffer('f', 3);
 	return true;
 }
-
+ 
 void render(BelaContext *context, void *userData)
 {
+	static int sample_count = 0;
+	//DataBuffer storred in 'buffer'
+	DataBuffer & buffer = gui.getDataBuffer(0);
+	// Retrieve contents of the buffer as floats
+	float * data = buffer.getAsFloat();
+ 
+	// values from sliders in Gui are storred in array data[]
+	transducer_processing.setResonantFrequencyHz(data[0]);
+	transducer_processing.setResonancePeakGainDb(data[1]);
+	transducer_processing.setResonanceQ(data[2]);
+	transducer_processing.setResonanceToneLevelDb(data[3]);
+	transducer_processing.setInductanceFilterCoefficient(data[4]);
+	transducer_processing.setTransducerInputWidebandGainDb(data[5]);
+	TransducerFeedbackCancellation::UnprocessedSamples unprocessed;
+	TransducerFeedbackCancellation::ProcessedSamples processed = transducer_processing.process(unprocessed);
+
+ 
 	for(unsigned int n = 0; n < context->audioFrames; ++n)
 	{
 		float randnum = (float) (rand() / (RAND_MAX / 2.0)) - 1.0;
-		TransducerFeedbackCancellation::UnprocessedSamples unprocessed;
-		unprocessed.output_to_transducer = audioRead(context, n, 2);
-		unprocessed.input_from_transducer = audioRead(context, n, 0);
-		unprocessed.reference_input_loopback = audioRead(context, n, 1);
-		
-		TransducerFeedbackCancellation::ProcessedSamples processed = transducer_processing.process(unprocessed);
-		
-		
-		
+		// TransducerFeedbackCancellation::UnprocessedSamples unprocessed;
+		unprocessed.output_to_transducer = audioRead(context, n, INPUT_ACTUATION_SIGNAL_PIN);
+		unprocessed.input_from_transducer = audioRead(context, n, INPUT_VOLTAGE_PIN);
+		unprocessed.reference_input_loopback = audioRead(context, n, INPUT_LOOPBACK_PIN);
+		// TransducerFeedbackCancellation::ProcessedSamples processed = transducer_processing.process(unprocessed);
 
+ 
 		// audioWrite(context, n, 0, processed.output_to_transducer * 0.2);
 		// audioWrite(context, n, 1, processed.output_to_transducer * 0.2);
 		// audioWrite(context, n, 2, processed.input_feedback_removed);
-		
-		
-		
-
-		audioWrite(context, n, 0, processed.output_to_transducer * 0.1 );
-		audioWrite(context, n, 1, processed.output_to_transducer * 0.1 );
-		
+ 
+		audioWrite(context, n, OUTPUT_AMP_PIN, processed.output_to_transducer * 0.1 );
+		audioWrite(context, n, OUTPUT_LOOPBACK_PIN, processed.output_to_transducer * 0.1 );
 		scope.log(processed.transducer_return_with_gain_applied, processed.modelled_signal, processed.input_feedback_removed);
-		
-		
-		
+		// rectifies the input signal
+		sample_t input_feedback_removed_rectified = abs(processed.input_feedback_removed);	
+		sample_t input_feedback_removed_rectified_lowpass = meter_filter.process(input_feedback_removed_rectified);
+		sample_count ++;
 
+		if (sample_count % 100 == 0) {
+			// sketch.js is implemenmted to create bar fropm a value from 0 - 1
+			gui.sendBuffer(0, map(input_feedback_removed_rectified_lowpass, 0, 2.3, 0, 1));
+			// rt_printf("%lf \n", input_feedback_removed_rectified_lowpass);
+		}
 	}
 }
-
+ 
 void cleanup(BelaContext *context, void *userData)
 {
 }
-
--- sketch.js
+++ sketch.js
@@ -1,31 +1,28 @@
 /*
-
+ 
+13/06/24
+ 
 !! Problems:
-	slider text needs to be limited to 2dp
-
+	n/a
+ 
 */
-
-
-
-
+ 
+ 
+ 
 //buffer to send to Bela. 6 elements: 6 sliders
 let buffer = [0,0,0,0,0,0];
-
-
-
+ 
+ 
 function setup() {
-	
 	// createCanvas(WW, WH);
 	// background(200);
-	
 	//Create 6 sliders for each of the 6 variables
-	ResonantFrequencyHz = createSlider(20, 1000, 20,0);
-	ResonancePeakGainDb = createSlider(0, 30, 0,0);
-	ResonanceQ = createSlider(0.5,20,0.5,0);
-	ResonanceToneLevelDb = createSlider(-90,0,-90,0);
-    InductanceFilterCoefficient = createSlider(0,1,0,0);
-    TransducerInputWidebandGainDb = createSlider(-20,20,-20,0);
-    
+	ResonantFrequencyHz = createSlider(20, 1000, 380,0.001);
+	ResonancePeakGainDb = createSlider(0, 30, 23.5,0.001);
+	ResonanceQ = createSlider(0.5,20,16.0,0.001);
+	ResonanceToneLevelDb = createSlider(-90,0,-10,0.001);
+    InductanceFilterCoefficient = createSlider(0,1,0.5,0.001);
+    TransducerInputWidebandGainDb = createSlider(-20,20,0,0.001);
 	//Text
 	p1 = createP("Resonant Frequency Hz:");
 	p2 = createP("Resonance Peak Gain Db:");
@@ -33,7 +30,6 @@ function setup() {
 	p4 = createP("Resonance Tone Level Db:");
 	p5 = createP("Inductance Filter Coefficient:");
     p6 = createP("Transducer Input Wideband Gain Db:");
-	
 	//Input textboxes 
     textBoxResFreq = createInput('');
     textBoxResPeak = createInput('');
@@ -41,43 +37,40 @@ function setup() {
     textBoxResTone = createInput('');
     textBoxInduct = createInput('');
     textBoxTransGain = createInput('');
-    
 	//This function will format colors and positions of the DOM elements 
 	formatDOMElements();
-
+ 
 }
-
-
-function draw() {  
-	
+ 
+ 
+function draw() {
 	// Retrieve the data being sent from render.cpp
 	let LevelMeter_value = Bela.data.buffers[0];			// the value between -1 and +1 
-	
-	// Draw a circle on the left hand side whose position changes with the values received from render.cpp
-	// !! could this be adapted to 
-	// ellipse(200 + windowWidth/3 + 100,
-	// 	windowHeight/2 + (((windowHeight/2)-100)*sine), 50, 50);
-	
-	let c2 = color(0);
-	
-	fill(c2);
-	rectMode(CORNERS)
-	rect(200 + windowWidth/3 + 100,								// bottom left corner x value
-		windowHeight/2 + ((windowHeight/2)-100),				// bottom left corner y value
-		100 + (200 + windowWidth/3 + 100),						// top right corner x value
-		windowHeight/2 + (((windowHeight/2)-100) * LevelMeter_value) + 1);	// top right corner y value 
-	
-	
-	let c1 = color(200);
-	
-	fill(c1);
-	rectMode(CORNERS)
-	rect(200 + windowWidth/3 + 100,								// same corners as above
-		windowHeight/2 + (((windowHeight/2)-100) * LevelMeter_value) + 1,
-		100 + (200 + windowWidth/3 + 100),
-		windowHeight/2 - ((windowHeight/2)-100));
-	
-	
+	let WH = windowHeight;
+
+	if (LevelMeter_value <= 1 && LevelMeter_value >= -1) {
+		clear();
+		let c2 = color(0);	//black
+		// the bar that rises 
+		fill(c2);
+		rectMode(CORNERS)
+		rect(200 + windowWidth/3 + 100,								// bottom left corner x value
+			WH - 100,										// bottom left corner y value
+			100 + (200 + windowWidth/3 + 100),						// top right corner x value
+			WH - (100 * (LevelMeter_value + 1)) - 100);			// top right corner y value 
+
+		let c1 = color(200); // grey
+		fill(c1);
+		rectMode(CORNERS)
+		rect(200 + windowWidth/3 + 100,								// same corners as above
+			WH - (100 * (LevelMeter_value + 1) + 100),
+			100 + (200 + windowWidth/3 + 100),
+			100);
+	}
+	else {
+		clear();
+	}
+
     //store values in the buffer
 	buffer[0] = ResonantFrequencyHz.value();
 	buffer[1] = ResonancePeakGainDb.value();
@@ -85,152 +78,115 @@ function draw() {
     buffer[3] = ResonanceToneLevelDb.value();
     buffer[4] = InductanceFilterCoefficient.value();
     buffer[5] = TransducerInputWidebandGainDb.value();
-
+ 
 	//SEND BUFFER to Bela. Buffer has index 0 (to be read by Bela),
 	//contains floats and sends the 'buffer' array.
     Bela.data.sendBuffer(0, 'float', buffer);
 }
-
+ 
 function formatDOMElements() {
-	
 	let WW = windowWidth;
 	let WH = windowHeight;
-	
 	let slider_width = 200;
 	let text_width = 100;
-	
 	createCanvas(WW, WH);
 	background(200);
-
+ 
 	//Format sliders repeated x6 
 	ResonantFrequencyHz.size(WW / 3);										//Sets the slider size to 1/3 the size of the window 
 	ResonantFrequencyHz.position(slider_width,						// set the slider to the right of the 
-        WH / 2 - 210);													//Sets the slider vertically 
-    
+        WH / 2 - 210);									//Sets the slider vertically 
     ResonancePeakGainDb.size(WW / 3);
     ResonancePeakGainDb.position(slider_width,
 		WH / 2 - 140);
-
+ 
     ResonanceQ.size(WW / 3);
     ResonanceQ.position(slider_width,
 		WH / 2 - 70);
-
+ 
     ResonanceToneLevelDb.size(WW / 3);
     ResonanceToneLevelDb.position(slider_width,
 		WH / 2 + 0);
-
+ 
     InductanceFilterCoefficient.size(WW / 3);
     InductanceFilterCoefficient.position(slider_width,
 		WH / 2 + 70);
-
+ 
     TransducerInputWidebandGainDb.size(WW / 3);
 	TransducerInputWidebandGainDb.position(slider_width,
 		WH / 2 + 140);
-	
 	// Format Text repeated x6
-    p1.position(slider_width,			// located to the same x as the slider 
+    p1.position(slider_width,		// located to the same x as the slider 
     	(WH / 2) - (210 + 40));					// located 40 above the slider 
-  
-    p2.position(slider_width, 
+    p2.position(slider_width,
     	(WH / 2) - (140 + 40));
-    	
-    p3.position(slider_width, 
-    	(WH / 2) - (70 + 40));					
-    
-    p4.position(slider_width, 
-    	(WH / 2) - (0 +40));						
-    
-    p5.position(slider_width, 
-    	(WH / 2) - (-70 + 40));						
-    
-    p6.position(slider_width, 
-    	(WH / 2) - (-140 + 40));					
-    	
-    	
-    	
+    p3.position(slider_width,
+    	(WH / 2) - (70 + 40));
+    p4.position(slider_width,
+    	(WH / 2) - (0 +40));
+    p5.position(slider_width,
+    	(WH / 2) - (-70 + 40));
+    p6.position(slider_width,
+    	(WH / 2) - (-140 + 40));
+
 	// Sorts positions of textbox and button 
 	// if textbox is imputted changes slider and vice-versa see functions updateValue() and sliderChange()
-	
     textBoxResFreq.position(text_width, 	// positions textbox to the left of the slider 
-    	WH / 2 - 210);	
+    	WH / 2 - 210);
     	// Positions textbox at the same height as the slider 
     textBoxResFreq.size(50);
-  
     ResonantFrequencyHz.input(sliderChange);
-    
     textBoxResFreq.value(ResonantFrequencyHz.value());
-    
     valueDisplayerResFreq = createP();
     valueDisplayerResFreq.position(30,height-50);
-    
     // repeat for ResonancePeakGainDb
-    textBoxResPeak.position(text_width, 
+    textBoxResPeak.position(text_width,
     	WH / 2 - 140);
     textBoxResPeak.size(50);
-  
     ResonancePeakGainDb.input(sliderChange);
-    
     textBoxResPeak.value(ResonancePeakGainDb.value());
-  
     valueDisplayerResPeak = createP();
     valueDisplayerResPeak.position(30,height-50);
-    
     // repeat for ResonanceQ
-    textBoxResQ.position(text_width, 
+    textBoxResQ.position(text_width,
     	WH / 2 - 70);
     textBoxResQ.size(50);
-  
     ResonanceQ.input(sliderChange);
-    
     textBoxResQ.value(ResonanceQ.value());
-  
     valueDisplayerResQ = createP();
     valueDisplayerResQ.position(30,height-50);
-    
     // repeat for ResonanceToneLevelDb
-    textBoxResTone.position(text_width, 
+    textBoxResTone.position(text_width,
     	WH / 2);
     textBoxResTone.size(50);
-  
     ResonanceToneLevelDb.input(sliderChange);
-    
     textBoxResTone.value(ResonanceToneLevelDb.value());
-  
     valueDisplayerResTone = createP();
     valueDisplayerResTone.position(30,height-50);
-    
     // repeat for InductanceFilterCoefficient
-    textBoxInduct.position(text_width, 
+    textBoxInduct.position(text_width,
     	WH / 2 + 70);
     textBoxInduct.size(50);
-  
     InductanceFilterCoefficient.input(sliderChange);
-    
     textBoxInduct.value(InductanceFilterCoefficient.value());
-  
     valueDisplayerInduct = createP();
     valueDisplayerInduct.position(30,height-50);
-    
     // repeat for TransducerInputWidebandGainDb
-    textBoxTransGain.position(text_width, 
+    textBoxTransGain.position(text_width,
     	WH / 2 + 140);
     textBoxTransGain.size(50);
-  
     TransducerInputWidebandGainDb.input(sliderChange);
-    
     textBoxTransGain.value(TransducerInputWidebandGainDb.value());
-  
     valueDisplayerTransGain = createP();
     valueDisplayerTransGain.position(30,height-50);
-	
 }
-
+ 
 function keyPressed() {
   if (keyCode === ENTER) {
     updateValue();
   }
 }
-
+ 
 function updateValue(){
 	//if the textbox is updated, update all the sliders
 	ResonantFrequencyHz.value(textBoxResFreq.value());
@@ -240,7 +196,7 @@ function updateValue(){
 	InductanceFilterCoefficient.value(textBoxInduct.value());
 	TransducerInputWidebandGainDb.value(textBoxTransGain.value());
 }
-
+ 
 function sliderChange(){
 	//if the slider is changed, update the textbox
 	textBoxResFreq.value(ResonantFrequencyHz.value());
