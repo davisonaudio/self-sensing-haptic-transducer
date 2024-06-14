d098c7818b3b2d59bd32e1f9b31a3c23c768fb66
Matt Davison <54866265+davisonaudio@users.noreply.github.com>
2024-06-14 10:02:12 +0100

    Moved method implementations out of TransducerFeedbackCancellation class

--- TransducerFeedbackCancellation.h
+++ TransducerFeedbackCancellation.h
@@ -36,34 +36,12 @@ public:
         AmplifierType amplifier_type;
     };
  
-    void setResonantFrequencyHz(sample_t resonant_frequency_hz)
-    {
-        m_resonance_filter.setCutoff(resonant_frequency_hz);
-    }
- 
-    void setResonancePeakGainDb(sample_t resonance_peak_gain_db)
-    {
-        m_resonance_filter.setFilterGain(resonance_peak_gain_db);   
-    }
- 
-    void setResonanceQ(sample_t resonance_q)
-    {
-        m_resonance_filter.setQ(resonance_q);
-    }
- 
-    void setResonanceToneLevelDb(sample_t resonance_tone_level_db)
-    {
-        m_resonance_tone.setLeveldB(resonance_tone_level_db);
-    }
-    void setInductanceFilterCoefficient(sample_t inductance_filter_coefficient)\
-    {
-        m_series_inductance_filter.setB1(inductance_filter_coefficient);
-    }
- 
-    void setTransducerInputWidebandGainDb(sample_t transducer_input_wideband_gain_db)
-    {
-        m_transducer_input_wideband_gain_lin = dBToLin(transducer_input_wideband_gain_db);
-    }
+    void setResonantFrequencyHz(sample_t resonant_frequency_hz);
+    void setResonancePeakGainDb(sample_t resonance_peak_gain_db);
+    void setResonanceQ(sample_t resonance_q);
+    void setResonanceToneLevelDb(sample_t resonance_tone_level_db);
+    void setInductanceFilterCoefficient(sample_t inductance_filter_coefficient);
+    void setTransducerInputWidebandGainDb(sample_t transducer_input_wideband_gain_db);
  
     struct UnprocessedSamples
     {
@@ -81,44 +59,9 @@ public:
     };
 
  
-    void setup(Setup setup_parameters)
-    {
-        //Setup of transducer resonance modelling filter, using peak biquad
-        Biquad::FilterSetup resonance_filter_setup;
-        resonance_filter_setup.cutoff_freq_hz = setup_parameters.resonant_frequency_hz;
-        resonance_filter_setup.filter_gain_db = setup_parameters.resonance_peak_gain_db;
-        resonance_filter_setup.quality_factor = setup_parameters.resonance_q;
-        resonance_filter_setup.sample_rate_hz = setup_parameters.sample_rate_hz;
-        resonance_filter_setup.filter_type = Biquad::FilterType::PEAK;
+    void setup(Setup setup_parameters);
  
-        m_resonance_filter.setup(resonance_filter_setup);
- 
-        //Setup of series inductance modelling filter (causes rise in impedance at HF) using onepole filter
-        m_series_inductance_filter.setB1(setup_parameters.inductance_filter_coefficient);
- 
-        m_transducer_input_wideband_gain_lin = dBToLin(setup_parameters.transducer_input_wideband_gain_db);
- 
-        //Setup of tone generator 
-        m_resonance_tone.setFrequency(setup_parameters.resonant_frequency_hz);
-        m_resonance_tone.setSampleRate(setup_parameters.sample_rate_hz);
-        m_resonance_tone.setLeveldB(setup_parameters.resonance_tone_level_db);
- 
-    }
- 
-    ProcessedSamples process(UnprocessedSamples unprocessed)
-    {
-        ProcessedSamples processed;
- 
-        //Process input from transducer
-        processed.modelled_signal = applyTransducerModelFilter(unprocessed.reference_input_loopback);
-        processed.transducer_return_with_gain_applied = m_transducer_input_wideband_gain_lin * unprocessed.input_from_transducer;
-        processed.input_feedback_removed = processed.transducer_return_with_gain_applied - processed.modelled_signal;
- 
-        //Process output to transducer
-        processed.output_to_transducer = unprocessed.output_to_transducer + m_resonance_tone.process();
- 
-        return processed;
-    }
+    ProcessedSamples process(UnprocessedSamples unprocessed);
  
 private:
  
@@ -127,10 +70,87 @@ private:
     ToneGenerator m_resonance_tone;
     sample_t m_transducer_input_wideband_gain_lin;
  
-    sample_t applyTransducerModelFilter(sample_t input_sample)
-    {
-        sample_t output = m_resonance_filter.process(input_sample);
-        output = m_series_inductance_filter.process(output);
-        return output;
-    }
+    sample_t applyTransducerModelFilter(sample_t input_sample);
 };
+
+/*
+* Implementation of methods
+*/
+
+
+void TransducerFeedbackCancellation::setResonantFrequencyHz(sample_t resonant_frequency_hz)
+{
+    m_resonance_filter.setCutoff(resonant_frequency_hz);
+}
+
+void TransducerFeedbackCancellation::setResonancePeakGainDb(sample_t resonance_peak_gain_db)
+{
+    m_resonance_filter.setFilterGain(resonance_peak_gain_db);   
+}
+
+void TransducerFeedbackCancellation::setResonanceQ(sample_t resonance_q)
+{
+    m_resonance_filter.setQ(resonance_q);
+}
+
+void TransducerFeedbackCancellation::setResonanceToneLevelDb(sample_t resonance_tone_level_db)
+{
+    m_resonance_tone.setLeveldB(resonance_tone_level_db);
+}
+void TransducerFeedbackCancellation::setInductanceFilterCoefficient(sample_t inductance_filter_coefficient)\
+{
+    m_series_inductance_filter.setB1(inductance_filter_coefficient);
+}
+
+void TransducerFeedbackCancellation::setTransducerInputWidebandGainDb(sample_t transducer_input_wideband_gain_db)
+{
+    m_transducer_input_wideband_gain_lin = dBToLin(transducer_input_wideband_gain_db);
+}
+ 
+
+void TransducerFeedbackCancellation::setup(Setup setup_parameters)
+{
+    //Setup of transducer resonance modelling filter, using peak biquad
+    Biquad::FilterSetup resonance_filter_setup;
+    resonance_filter_setup.cutoff_freq_hz = setup_parameters.resonant_frequency_hz;
+    resonance_filter_setup.filter_gain_db = setup_parameters.resonance_peak_gain_db;
+    resonance_filter_setup.quality_factor = setup_parameters.resonance_q;
+    resonance_filter_setup.sample_rate_hz = setup_parameters.sample_rate_hz;
+    resonance_filter_setup.filter_type = Biquad::FilterType::PEAK;
+
+    m_resonance_filter.setup(resonance_filter_setup);
+
+    //Setup of series inductance modelling filter (causes rise in impedance at HF) using onepole filter
+    m_series_inductance_filter.setB1(setup_parameters.inductance_filter_coefficient);
+
+    m_transducer_input_wideband_gain_lin = dBToLin(setup_parameters.transducer_input_wideband_gain_db);
+
+    //Setup of tone generator 
+    m_resonance_tone.setFrequency(setup_parameters.resonant_frequency_hz);
+    m_resonance_tone.setSampleRate(setup_parameters.sample_rate_hz);
+    m_resonance_tone.setLeveldB(setup_parameters.resonance_tone_level_db);
+
+}
+
+ProcessedSamples TransducerFeedbackCancellation::process(UnprocessedSamples unprocessed)
+{
+    ProcessedSamples processed;
+
+    //Process input from transducer
+    processed.modelled_signal = applyTransducerModelFilter(unprocessed.reference_input_loopback);
+    processed.transducer_return_with_gain_applied = m_transducer_input_wideband_gain_lin * unprocessed.input_from_transducer;
+    processed.input_feedback_removed = processed.transducer_return_with_gain_applied - processed.modelled_signal;
+
+    //Process output to transducer
+    processed.output_to_transducer = unprocessed.output_to_transducer + m_resonance_tone.process();
+
+    return processed;
+}
+
+sample_t TransducerFeedbackCancellation::applyTransducerModelFilter(sample_t input_sample)
+{
+    sample_t output = m_resonance_filter.process(input_sample);
+    output = m_series_inductance_filter.process(output);
+    return output;
+}
+
